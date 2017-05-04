68e5729abf1af745e0f808c7c0146028fa7262fb
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2017-05-04 10:14:47 +0900

    Start learning shapeless

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "learning-shapeless"
+scalaVersion := "2.12.2"
+
+libraryDependencies ++= Seq("com.chuusai" %% "shapeless" % "2.3.2")
---
+++ src/main/scala/yokohama/murataku/Ch1.scala
@@ -0,0 +1,40 @@
+package yokohama.murataku
+
+import shapeless._
+
+case class Employee(name: String, number: Int, manager: Boolean)
+case class IceCream(name: String, numCherries: Int, inCone: Boolean)
+
+case class Red()
+case class Amber()
+case class Green()
+
+trait CsvEncoder[A] {
+  def encode(value: A): List[String]
+}
+
+sealed trait Shape
+final case class Rectangle(width: Double, height: Double) extends Shape
+final case class Circle(radius: Double) extends Shape
+
+object Ch1 extends App {
+  println("moeeeee!!!")
+
+  lazy val genericEmp = Generic[Employee].to(Employee("Ohta", 33, false))
+  lazy val genericIce = Generic[IceCream].to(IceCream("Vanilla", 0, true))
+
+  def genericCsv(gen: String :: Int :: Boolean :: HNil): List[String] = 
+    List(gen(0), gen(1), gen(2)).map(_.toString)
+
+  println(genericCsv(genericEmp))
+
+  println(Generic[IceCream].from(genericEmp))
+
+  type Light = Red :+: Amber :+: Green :+: CNil
+
+  val red: Light = Inl(Red())
+  val amber: Light = Inr(Inl(Amber()))
+  val green: Light = Inr(Inr(Inl(Green())))
+
+  val gen = Generic[Shape]
+}
